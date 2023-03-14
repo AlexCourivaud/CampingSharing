b33b6d70004e49809efdd388712b45aceed0d9bf
Alexandre Courivaud <alexandre.courivaud@gmail.com>
2023-03-14 18:14:05 +0100

    modif routes + début css

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'bookings/destroy'
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/edit'
-  get 'products/update'
-  get 'products/destroy'
   devise_for :users
   root to: "pages#home"
+
+  resources :products, only: [:index, :show]
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
